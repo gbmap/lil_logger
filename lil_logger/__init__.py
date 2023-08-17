5ff87959d3d7713c72e64875f769f7cdc6929017
gbmap <gabehilism@gmail.com>
2023-08-17 04:01:59 -0300

    help

--- lil_logger/__init__.py
+++ lil_logger/__init__.py
@@ -1,10 +1,19 @@
 """
-Ridiculously lazy logger. Just a wrapper to Python's `logging`
-with color support.
+Ridiculously lazy logger. Just a wrapper for Python's `logging` with color support.
+```
+    >>> log = lil_logger.get("logger name", lil_logger.DEBUG)
+    >>> log.info("this is useful information")
+```
 """
 
 import logging
 from colorama import Fore, Back
+
+DEBUG = logging.DEBUG
+INFO = logging.INFO
+WARNING = logging.WARNING
+ERROR = logging.ERROR
+CRITICAL = logging.CRITICAL
 
 class LilFormatter(logging.Formatter):
     LOG_FORMAT = "[%(asctime)s][%(levelname)s] %(message)s"
@@ -24,6 +33,14 @@ def get(name: str, level: int, log_format:str = None) -> logging.Logger:
     """
     Creates a :class:`logging.Logger` with the specified configurations,
     using colors for different message types.
+
+    Args:
+        name (str): Name of the logger.
+        level (int): Level of info to output.
+        log_format (str): Optional message formatting. Defaults to "[YYYY-MM-DD HH:mm:ss,sss][LEVEL] message".
+
+    Returns
+        logging.Logger: logger instance with formatting already set.
     """
     logger = logging.getLogger(name)
     logger.setLevel(level)
